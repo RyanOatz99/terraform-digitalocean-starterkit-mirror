ee09ec97414d77bf2b377c3465e28a2de39504a3
Tom Vaughan <thomas.david.vaughan@gmail.com>
2018-03-25 10:14:18 +0200

    Create main.tf.
    Because of
    https://www.terraform.io/docs/modules/create.html#standard-module-structure

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 # -*- coding: utf-8; mode: terraform -*-
 
+provider "digitalocean" {
+  # https://www.terraform.io/docs/providers/do/index.html
+  version = "~> 0.1"
+}
+
+resource "digitalocean_ssh_key" "starterkit_ssh_pubkey" {
+  name       = "starterkit"
+  public_key = "${var.starterkit_ssh_pubkey}"
+}
+
 resource "digitalocean_droplet" "starterkit_droplet" {
   image    = "${var.starterkit_droplet["image"]}"
   name     = "${var.starterkit_droplet["name"]}"
