c7dd7bd9872f5361d3e7e01ddea8572c563602ca
Tom Vaughan <thomas.david.vaughan@gmail.com>
2018-10-28 08:33:02 +0100

    Create droplet.tf.

---
+++ droplet.tf
@@ -0,0 +1,14 @@
+# -*- coding: utf-8; mode: terraform -*-
+
+resource "digitalocean_ssh_key" "starterkit_ssh_pubkey" {
+  name       = "starterkit"
+  public_key = "${var.starterkit_ssh_pubkey}"
+}
+
+resource "digitalocean_droplet" "starterkit_droplet" {
+  image    = "${var.starterkit_droplet["image"]}"
+  name     = "${var.starterkit_droplet["name"]}"
+  region   = "${var.starterkit_droplet["region"]}"
+  size     = "${var.starterkit_droplet["size"]}"
+  ssh_keys = ["${digitalocean_ssh_key.starterkit_ssh_pubkey.id}"]
+}
--- main.tf
+++ main.tf
@@ -4,16 +4,3 @@ provider "digitalocean" {
   # https://www.terraform.io/docs/providers/do/index.html
   version = "~> 1.0"
 }
-
-resource "digitalocean_ssh_key" "starterkit_ssh_pubkey" {
-  name       = "starterkit"
-  public_key = "${var.starterkit_ssh_pubkey}"
-}
-
-resource "digitalocean_droplet" "starterkit_droplet" {
-  image    = "${var.starterkit_droplet["image"]}"
-  name     = "${var.starterkit_droplet["name"]}"
-  region   = "${var.starterkit_droplet["region"]}"
-  size     = "${var.starterkit_droplet["size"]}"
-  ssh_keys = ["${digitalocean_ssh_key.starterkit_ssh_pubkey.id}"]
-}
